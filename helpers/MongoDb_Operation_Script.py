ec1fc0e2e41495cc81865fecb23b742987a51c70
Jyoti Sharma <70335421+jsharmashell@users.noreply.github.com>
2025-01-30 14:06:51 +0530

    Update MongoDb_Operation_Script.py
    remove url

--- helpers/MongoDb_Operation_Script.py
+++ helpers/MongoDb_Operation_Script.py
@@ -1,7 +1,7 @@
 
 import pandas as pd
 from pymongo import MongoClient
-mongo_client = MongoClient("mongodb+srv://ssharma:saritapassword@b2b-backbone-test.3wiap.mongodb.net/")
+mongo_client = MongoClient("mongodbsrvuri")
 mongo_db = mongo_client.get_database("eu_b2b")
 
 collection = mongo_db.consumption_de_p_kwh_custom # collection name
